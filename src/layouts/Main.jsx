be193a33615727002faec972fea0773e6425f55e
MUKHTER <mohammadmukhter@gmail.com>
2023-05-24 13:00:03 +0600

    nav section and banner section design done

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
+      <NavBar></NavBar>
       <Outlet></Outlet>
     </div>
   );
