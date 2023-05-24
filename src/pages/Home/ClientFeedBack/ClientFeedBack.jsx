db863dc0a4183d35b709c19e8d9408cec47a827e
MUKHTER <mohammadmukhter@gmail.com>
2023-05-25 02:12:53 +0600

    client feedback

---
+++ src/pages/Home/ClientFeedBack/ClientFeedBack.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const ClientFeedBack = () => {
+  return (
+    <div>
+      <SectionTitle
+        subHeading="What Our Clients Says"
+        heading="Testimonials"
+      ></SectionTitle>
+      <h2>This is Client feed back section</h2>
+    </div>
+  );
+};
+
+export default ClientFeedBack;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Banner/Banner";
 import CallUsSection from "../CallUsSection/CallUsSection";
 import ChefRecommend from "../ChefRecommend/ChefRecommend";
+import ClientFeedBack from "../ClientFeedBack/ClientFeedBack";
 import MenuFeatured from "../MenuFeatured/MenuFeatured";
 import OrderOnlineSection from "../OrderOnlineSection/OrderOnlineSection";
 import PopularMenu from "../PopularMenu/PopularMenu";
@@ -18,6 +19,9 @@ const Home = () => {
         <ChefRecommend></ChefRecommend>
       </div>
       <MenuFeatured></MenuFeatured>
+      <div className="mx-12 md:mx-28">
+        <ClientFeedBack></ClientFeedBack>
+      </div>
     </div>
   );
 };
