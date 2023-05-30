b29ae4393bb7e24e98378bc4ba408fb642ba14e5
MUKHTER <mohammadmukhter@gmail.com>
2023-05-30 13:06:43 +0600

    add react hook form in register page and validation

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,13 +1,20 @@
+import { useForm } from "react-hook-form";
 import regImg from "../../../assets/others/authentication2.png";
 
 const Register = () => {
-  const formHandler = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const email = form.email.value;
-    const password = form.password.value;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    console.log({ email, password });
+  const formHandler = (data) => {
+    // event.preventDefault();
+    // const form = event.target;
+    // const email = form.email.value;
+    // const password = form.password.value;
+
+    console.log(data);
   };
 
   return (
@@ -19,17 +26,20 @@ const Register = () => {
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow bg-base-100/30">
             <div className="card-body">
-              <form onSubmit={formHandler}>
+              <form onSubmit={handleSubmit(formHandler)}>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Name</span>
                   </label>
                   <input
                     type="text"
-                    name="name"
+                    {...register("name", { required: true })}
                     placeholder="Name"
                     className="input input-bordered"
                   />
+                  <span className="text-red-600 text-xs ms-2 mt-1">
+                    {errors.name && <p>Name is required.</p>}
+                  </span>
                 </div>
                 <div className="form-control">
                   <label className="label">
@@ -37,10 +47,13 @@ const Register = () => {
                   </label>
                   <input
                     type="text"
-                    name="photoUrl"
+                    {...register("photoUrl", { required: true })}
                     placeholder="Photo URL"
                     className="input input-bordered"
                   />
+                  <span className="text-red-600 text-xs ms-2 mt-1">
+                    {errors.photoUrl && <p>Photo URL is required.</p>}
+                  </span>
                 </div>
                 <div className="form-control">
                   <label className="label">
@@ -48,10 +61,13 @@ const Register = () => {
                   </label>
                   <input
                     type="email"
-                    name="email"
+                    {...register("email", { required: true })}
                     placeholder="email"
                     className="input input-bordered"
                   />
+                  <span className="text-red-600 text-xs ms-2 mt-1">
+                    {errors.email && <p>Email is required.</p>}
+                  </span>
                 </div>
                 <div className="form-control">
                   <label className="label">
@@ -59,15 +75,24 @@ const Register = () => {
                   </label>
                   <input
                     type="password"
-                    name="password"
+                    {...register("password", {
+                      required: true,
+                      pattern: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8}/,
+                    })}
                     placeholder="password"
                     className="input input-bordered"
                   />
-                  <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">
-                      Forgot password?
-                    </a>
-                  </label>
+                  <span className="text-red-600 text-xs ms-2 mt-1">
+                    {errors.password?.type === "required" && (
+                      <p>Password is required.</p>
+                    )}
+                    {errors.password?.type === "pattern" && (
+                      <p>
+                        Password must be 8 character and at least one upper
+                        letter and one small letter one number.
+                      </p>
+                    )}
+                  </span>
                 </div>
 
                 <div className="form-control mt-6">
